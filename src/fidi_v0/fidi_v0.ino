5b3c79cdeafba573bb8bfdb075e7587464d57d45
the-magister <magister@the-magister.com>
2016-10-06 09:51:39 -0700

    broke out the MIDI metronome code into a class for clarity (I hope)

--- src/fidi_v0/fidi_v0.ino
+++ src/fidi_v0/fidi_v0.ino
@@ -15,10 +15,54 @@ Bounce Armed = Bounce(ARM_BUTTON, 3UL);
 #define SOLENOID_OFF LOW
 
 // hysteresis handling (i.e. "lag")
-#define MIN_HYST_TIME 0UL // microseconds
-#define MAX_HYST_TIME 1000UL*1000UL // microseconds
+#define MIN_HYST_TIME 0UL // ms
+#define MAX_HYST_TIME 1000UL // ms
 
 Solenoid Fire;
+
+class Beats {
+  public:
+    // set the interval in ms
+    void tempo( unsigned long interval );
+    // set start of interval
+    void start();
+
+    // return TRUE at beat start
+    boolean isBeat();
+    // return the number of ms until the next beat
+    unsigned long nextBeat();
+    // return the total cycle time in ms
+    unsigned long getTempo();
+
+  private:
+    unsigned long interval, lastBeat;
+
+    Metro beatCounter;
+};
+
+void Beats::tempo( unsigned long interval ) {
+  this->interval = interval;
+  this->beatCounter.interval( interval );
+}
+void Beats::start() {
+  this->beatCounter.reset();
+  this->lastBeat = millis();
+}
+boolean Beats::isBeat() {
+  if( beatCounter.check() ) {
+    this->lastBeat = millis();
+    return( true );
+  } else {
+    return( false );
+  }
+}
+unsigned long Beats::nextBeat() {
+  return( this->interval - ( millis() - this->lastBeat ) );
+}
+unsigned long Beats::getTempo() {
+  return( this->interval );
+}
+Beats Beat;
 
 void setup() {
   Serial.begin(115200);
@@ -28,22 +72,25 @@ void setup() {
 
   // Initialize Solenoid
   Fire.begin(SOLENOID_PIN, SOLENOID_OFF);
+
+  // Initialize Beat
+  Beat.tempo( 461UL );
+  Beat.start();
 }
 
+
 void loop() {
+  // check in with the MIDI signal metronome
+  boolean isBeat = Beat.isBeat(); // important to keep checking AND CLEARING this.
+  unsigned long cycleTime = Beat.getTempo(); 
 
   // Read slider value from "A0/D10" position and map to hysteresis times
   unsigned long laggedValue = map(analogRead(SLIDER1), 0, 1023, MIN_HYST_TIME, MAX_HYST_TIME);
 
-  // Read something from the MIDI side of things and map to total cycle time
-  unsigned long cycleTime = 461UL;
-  static Metro beatCounter(cycleTime);
-  //beatCounter.interval(cycleTime);
-  boolean isBeat = beatCounter.check(); // important to keep checking AND CLEARING this.
-  
   // Read slider value from "A1/D11" position and map to fraction of the total cycle time
   unsigned long onTime = map(analogRead(SLIDER2), 0, 1023, 0, cycleTime);
 
+  // use an LED to track the beat
   static Metro turnOffBeatLED(10UL);
   if( isBeat ) {
       turnOffBeatLED.interval(onTime);
@@ -63,14 +110,15 @@ void loop() {
 
       // show the timings
       Fire.show();
-      unsigned long delayFor = cycleTime * 1000UL - laggedValue;
-      Serial << F("Lag settings.  lag=") << laggedValue << F(" us. delayFor=") << delayFor << F(" us.") << endl;
-
-      // I think the simplest thing to do is wait here.
-      // wait for the next beat
-      while ( !beatCounter.check() );
-      // wait cycleTime - lag
-      delayMicroseconds( delayFor );
+ 
+      // can we get in on the next beat in time?
+      if( Beat.nextBeat() < laggedValue ) {
+        // no
+        // wait for the next beat
+        while ( !Beat.isBeat() );
+      }
+      // get ready
+      delay( Beat.nextBeat() - laggedValue );
       // Arm
       Fire.arm();
       // solenoid will trigger on next .update()
